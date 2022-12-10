dd945f2c71cbc1c020845b69ea5157713865f1ea
JustDrven <petr.nemec11111@gmail.com>
2022-12-10 14:07:48 +0100

    Update panel.php

--- panel.php
+++ panel.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="./src/css/image.css">
-    <link rel="stylesheet" href="./src/css/ranks.css">
     <link rel="stylesheet" href="./src/css/main.css">
+    <link rel="stylesheet" href="./src/css/ranks.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MIdecon.eu | InfoPanel</title>
@@ -13,6 +13,8 @@
 <body>
     <div>
         <?php session_start();
+
+
 
             if (isset($_SESSION["BungeeUUID"])) {
 
@@ -33,47 +35,52 @@
             }
         ?>
     </div>
-    <?php 
-    
-    if ($_SESSION["BungeeRank"] == "Majitel") {
-        echo "    <div class=\"tickets-list\">
-        <table class=\"table table-striped\" style=\"width: 40rem;\">
-            <thead>
-                <tr>
-                    <th>ID:</th>
-                    <th>Player:</th>
-                    <th>Reason:</th>
-                    <th>Status:</th>
-                </tr>
-            </thead>
-            <tbody>
-            <tr>
-                    <td>1</td>
-                    <td>Test</td>
-                    <td>Povolit VPN</td>
-                    <td><button class=\"btn btn-warning\">Čekání na odpoveď ATeamu</button></td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Test323</td>
-                    <td>Povolit VPN</td>
-                    <td><button class=\"btn btn-warning\">Čekání na odpoveď ATeamu</button></td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Test4345</td>
-                    <td>Povolit VPN</td>
-                    <td><button class=\"btn btn-warning\">Čekání na odpoveď ATeamu</button></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>";
-    } else {
-        return;
-    }
-    
-    ?>
 
+
+        <?php
+
+            // $ranks = array("Majitel", "Leader");
+
+            $rank = [
+                    "helpers" => [
+                        "Majitel",
+                        "Leader"
+                    ]
+            ];
+
+        if (in_array($_SESSION["BungeeRank"], $rank["helpers"])) {
+            echo "<div class=\"tickets-list\">
+                <table class=\"table table-striped\" style=\"width: 40rem;\">
+                    <thead>
+                        <tr>
+                            <th>ID:</th>
+                            <th>Player:</th>
+                            <th>Reason:</th>
+                            <th>Status:</th>
+                        </tr>
+                    </thead>
+                <tbody>";
+                        $connected = mysqli_connect("localhost", "root", "", "tickets-list");
+                        $result = mysqli_query($connected, "SELECT * FROM `list`");
+
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "<tr>
+                            <td>". $row["id"] ."</td>
+                            <td>Test4345</td>
+                            <td>Povolit VPN</td>
+                            <td><button class=\"btn btn-warning\">Čekání na odpoveď ATeamu</button></td>
+                        </tr>";
+                        }
+
+                        echo "</tr>
+                        </tbody>
+                    </table>
+                </div>";
+
+                             
+            }
+                    
+            ?>
 
 </body>
 </html>
