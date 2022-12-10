50909963a753ac1c4b70dfe9aef5c92c85715886
JustDrven <petr.nemec11111@gmail.com>
2022-12-10 11:21:04 +0100

    To panel.php file was added image, ranksystem and bootstrap

--- panel.php
+++ panel.php
@@ -1,21 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cz">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="./src/css/image.css">
+    <link rel="stylesheet" href="./src/css/ranks.css">
+    <link rel="stylesheet" href="./src/css/main.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MIdecon.eu | InfoPanel</title>
 </head>
 <body>
-    <?php session_start();
+    <div>
+        <?php session_start();
 
 
-    
-        echo "<img src=\"https://visage.surgeplay.com/bust/96/". $_SESSION["BungeeUUID"] ."\" alt=\"Logo\"> <br />";
-        echo $_SESSION["BungeeName"]. "<br />";
-        echo "Rank: ". $_SESSION["BungeeRank"];
-    
+            echo "<nav class='navigation'>";
+            echo "<div class='logo'>";
+            echo "<img src='./src/images/logo.png' alt='Logo'>";
+            echo "</div>";
+            echo "<div class='card-infopanel'>";
+            echo "<img src=\"https://visage.surgeplay.com/bust/96/". $_SESSION["BungeeUUID"] ."\" alt=\"Logo\">";
+            echo "<div class='card-infopanel-text'>";
+            echo "<p>".$_SESSION["BungeeName"]. "</p>";
+            echo "<p class=" . $_SESSION["BungeeRank"] . ">". $_SESSION["BungeeRank"], "</p>";
+            echo "</div>";
+            echo "</div>";
+            echo "</nav>";
+        ?>
+    </div>
 
-    ?>
 </body>
 </html>
